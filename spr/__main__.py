36bebd01be670f2394fb1c4e8ba883355ef42510
Tosu <terebina860@gmail.com>
2024-12-27 05:44:52 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -41,7 +41,7 @@ async def main():
     await spr.stop()
 
 
-@spr.on_message(filters.command(["help", "start"]), group=2)
+@spr.on_message(filters.command("help", "start"), group=2)
 async def help_command(_, message: Message):
     if message.chat.type != "private":
         kb = ikb({"Help": f"https://t.me/{BOT_USERNAME}?start=help"})
