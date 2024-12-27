fd72730139d2b8b176f7e7f3ec9b305960403a61
Tosu <terebina860@gmail.com>
2024-12-27 05:37:38 +0530

    Update functions.py

--- spr/utils/functions.py
+++ spr/utils/functions.py
@@ -105,7 +105,7 @@ __Message has been deleted__
         {
             "Correct (0)": "upvote_spam",
             "Incorrect (0)": "downvote_spam",
-            "Chat": "https://t.me/" + (message.chat.username or "SpamProtectionLog/93"),
+            "Chat": "https://t.me/" + (message.chat.username or "Alice_x_Hub"),
         },
         2
     )
